0b3cbf6ed58135f32367529366f9d94a8d139f9d
Arnošt Havelka <arnost.havelka@gmail.com>
2015-08-26 16:32:59 +0200

    + error handling (mapping to custom JSPs)

--- src/main/java/com/github/aha/poc/security/config/MvcConfig.java
+++ src/main/java/com/github/aha/poc/security/config/MvcConfig.java
@@ -1,6 +1,11 @@
 package com.github.aha.poc.security.config;
 
+import org.springframework.boot.context.embedded.ConfigurableEmbeddedServletContainer;
+import org.springframework.boot.context.embedded.EmbeddedServletContainerCustomizer;
+import org.springframework.boot.context.embedded.ErrorPage;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
@@ -12,11 +17,6 @@ import com.github.aha.poc.security.controller.ActionConsts;
 @Configuration
 public class MvcConfig extends WebMvcConfigurerAdapter {
 
-//	@Override
-//	public void configureViewResolvers(ViewResolverRegistry registry) {
-//		registry.jsp("/WEB-INF/views/", ".jsp");
-//	}	
-	
 	@Override
     public void addViewControllers(ViewControllerRegistry registry) {
     	registry.addViewController(ActionConsts.ROOT).setViewName("welcomePage");
@@ -24,5 +24,16 @@ public class MvcConfig extends WebMvcConfigurerAdapter {
         registry.addViewController(ActionConsts.HOME).setViewName("homePage");
         registry.addViewController(ActionConsts.USER).setViewName("userPage");
         registry.addViewController(ActionConsts.ADMIN).setViewName("adminPage");
+        registry.addViewController(ActionConsts.ERROR_403).setViewName("error/error403");
     }
+	
+	@Bean
+	public EmbeddedServletContainerCustomizer containerCustomizer() {
+        return new EmbeddedServletContainerCustomizer() {
+            @Override
+            public void customize(ConfigurableEmbeddedServletContainer container) {
+                container.addErrorPages(new ErrorPage(HttpStatus.FORBIDDEN, ActionConsts.ERROR_403));
+            }
+        };
+	}	
 }
--- src/main/java/com/github/aha/poc/security/controller/ActionConsts.java
+++ src/main/java/com/github/aha/poc/security/controller/ActionConsts.java
@@ -13,5 +13,7 @@ public class ActionConsts {
     public static final String USER = "/user";
     
     public static final String ADMIN = "/admin";
+    
+    public static final String ERROR_403 = "/error403";
 
 }
