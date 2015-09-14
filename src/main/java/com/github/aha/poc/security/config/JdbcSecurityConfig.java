3ac9bb201e94724f2dcdc49c84222f9f88c64746
Arnošt Havelka <arnost.havelka@gmail.com>
2015-09-14 15:10:31 +0200

    + security config from JDBC

---
+++ src/main/java/com/github/aha/poc/security/config/JdbcSecurityConfig.java
@@ -0,0 +1,114 @@
+package com.github.aha.poc.security.config;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+@Configuration
+@Profile("JDBC")
+public class JdbcSecurityConfig extends AbstractSecurityConfig {
+	
+	@Autowired
+	private JdbcTemplate jdbcTemplate;
+
+	@Autowired
+	public void configureGlobal(AuthenticationManagerBuilder authenticationMgr) throws Exception {
+		authenticationMgr.userDetailsService(new JdbcUserService(jdbcTemplate));
+	}
+
+	class JdbcUserService implements UserDetailsService {
+
+		private JdbcTemplate jdbcTemplate;
+
+		public JdbcUserService(JdbcTemplate jdbcTemplate) {
+			this.jdbcTemplate = jdbcTemplate;
+
+		}
+
+		@Override
+		public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+			// load user details
+			UserDTO user = jdbcTemplate.queryForObject(
+					"select id, username, passwd from u_principal where username = ?", new Object[] { username }, new UserMapper());
+
+			if (user == null) {
+				throw new UsernameNotFoundException(String.format("userma=%s", username));
+			}
+			// load authorities
+			List<GrantedAuthority> authorities = new ArrayList<GrantedAuthority>();
+			List<Map<String, Object>> data = jdbcTemplate.queryForList("select name from u_role where user_id = :id", user.getId());
+			for (Map<String, Object> item : data) {
+				String role = String.format("ROLE_%s", item.get("NAME"));
+				authorities.add(new SimpleGrantedAuthority(role));
+			}
+
+			return new User(user.getUsername(), user.getPasswd(), authorities);
+		}
+
+	}
+
+	class UserMapper implements RowMapper<UserDTO> {
+
+		@Override
+		public UserDTO mapRow(ResultSet rs, int rowNum) throws SQLException {
+			return new UserDTO(rs.getInt("id"), rs.getString("username"), rs.getString("passwd"));
+		}
+
+	}
+
+	class UserDTO {
+
+		public UserDTO(int id, String username, String passwd) {
+			super();
+			this.id = id;
+			this.username = username;
+			this.passwd = passwd;
+		}
+
+		private int id;
+
+		private String username;
+
+		private String passwd;
+
+		public int getId() {
+			return id;
+		}
+
+		public void setId(int id) {
+			this.id = id;
+		}
+
+		public String getUsername() {
+			return username;
+		}
+
+		public void setUsername(String username) {
+			this.username = username;
+		}
+
+		public String getPasswd() {
+			return passwd;
+		}
+
+		public void setPasswd(String passwd) {
+			this.passwd = passwd;
+		}
+
+	}
+}
---
+++ src/main/resources/db/changelog/db.changelog-master.sql
@@ -0,0 +1,26 @@
+--liquibase formatted sql
+
+--changeset aha:1
+create table u_principal (
+    id int primary key,
+    username varchar(50),
+    passwd varchar(50)
+);
+--rollback drop table u_principal;
+
+--changeset aha:2
+insert into u_principal(id, username, passwd) values (1, 'aha', 'aha');
+insert into u_principal(id, username, passwd) values (2, 'admin', 'admin');
+
+--changeset aha:3
+create table u_role (
+    id int primary key,
+    user_id int,
+    name varchar(50)
+);
+--rollback drop table u_role;
+
+--changeset aha:4
+insert into u_role(id, user_id, name) values (1, 1, 'USER');
+insert into u_role(id, user_id, name) values (2, 2, 'DEVELOPERS');
+insert into u_role(id, user_id, name) values (3, 2, 'ADMIN');
