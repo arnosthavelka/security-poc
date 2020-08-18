e3df70d39bcf762d24365c453b262edc5c1f1e02
hh310 <arnost.havelka@deutsche-boerse.com>
2020-08-18 08:22:50 +0200

    - Sonar hint

--- src/main/java/com/github/aha/poc/security/controller/ActionConsts.java
+++ src/main/java/com/github/aha/poc/security/controller/ActionConsts.java
@@ -2,18 +2,22 @@ package com.github.aha.poc.security.controller;
 
 public class ActionConsts {
 
+	private ActionConsts() {
+		throw new IllegalStateException("Utility class");
+	}
+
 	public static final String ROOT = "/";
-	
+
 	public static final String LOGIN = "/login";
-	
-    public static final String WELCOME = "/welcome";
-    
-    public static final String HOME = "/home";
-    
-    public static final String USER = "/user";
-    
-    public static final String ADMIN = "/admin";
-    
-    public static final String ERROR_403 = "/error403";
+
+	public static final String WELCOME = "/welcome";
+
+	public static final String HOME = "/home";
+
+	public static final String USER = "/user";
+
+	public static final String ADMIN = "/admin";
+
+	public static final String ERROR_403 = "/error403";
 
 }
